85f48d6636fc6e04812a70e83491740def0e99b1
chrkb1569 <chrkb1569@naver.com>
2022-10-06 14:53:44 +0900

    INIT PROJECT

---
+++ src/main/java/com/example/demo/MyongJiTalkTalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyongJiTalkTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyongJiTalkTalkApplication.class, args);
+	}
+
+}
