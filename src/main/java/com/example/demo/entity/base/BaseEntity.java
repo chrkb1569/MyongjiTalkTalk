ef7c6eb3838bd77a03b66601dd8dd583d7114677
chrkb1569 <chrkb1569@naver.com>
2022-10-13 13:37:43 +0900

    feat : Add Auditing files

---
+++ src/main/java/com/example/demo/entity/base/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.example.demo.entity.base;
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
