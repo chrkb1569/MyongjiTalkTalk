53da964c555038902babd018a8bdeacff8693b08
chrkb1569 <chrkb1569@naver.com>
2022-11-06 19:59:58 +0900

    feat : Edit Board Dto Files

--- src/main/java/com/example/demo/dto/board/BoardListResponseDto.java
+++ src/main/java/com/example/demo/dto/board/BoardListResponseDto.java
@@ -1,9 +1,13 @@
 package com.example.demo.dto.board;
 
+import com.example.demo.dto.comment.CommentDto;
 import com.example.demo.entity.board.Board;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
--- src/main/java/com/example/demo/dto/board/BoardResponseDto.java
+++ src/main/java/com/example/demo/dto/board/BoardResponseDto.java
@@ -1,12 +1,32 @@
 package com.example.demo.dto.board;
 
+import com.example.demo.dto.comment.CommentDto;
+import com.example.demo.entity.board.Board;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BoardResponseDto {
     private String title;
     private String content;
     private String writer;
+    private List<CommentDto> comment;
+
+    public BoardResponseDto(Board board) {
+        this.title = board.getTitle();
+        this.content = board.getContent();
+        this.writer = board.getWriter();
+    }
+
+    public BoardResponseDto(Board board, List<CommentDto> lst) {
+        this.title = board.getTitle();
+        this.content = board.getContent();
+        this.writer = board.getWriter();
+        this.comment = lst;
+    }
 }
